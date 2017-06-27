42367fdc6c18de9c28c6775a9b4171f8c2e7c952
JoseVillarroel <jose06villarroel@gmail.com>
2017-06-26 23:09:36 -0400

    prueba pasar, resivir y mostrar datos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,64 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController) {
 
   }
+  presentAlert(){
+    let alert = this.alertCtrl.create({
+      title: 'Esto es una alerta de prueba.',
+      subTitle: 'Es de prueba.',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
+  login(){
+    let alert = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username'
+        },
+        {
+          name: 'pass',
+          placeholder: 'Pass',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            if(data.username == "admin" && data.pass == "123456"){
+              //logeado
+              this.navCtrl.push(LoginPage, {
+                name: data.username,
+                pass: data.pass
+              });
+            }else{
+              //not today
+              let alert = this.alertCtrl.create({
+                title: 'Usuario o contraceña invalida.',
+                subTitle: 'Intente de nuevo.',
+                buttons: ['OK']
+              });
+              alert.present();
+              return false;
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
